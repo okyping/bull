0dbe07314742bb104abd2eb6037e435cff2da566
liyinan <liyinan@baidu.com>
2015-04-02 13:42:30 +0800

    add import test file

---
+++ demo/src/importConfig.js
@@ -0,0 +1,13 @@
+/**
+ * @file 配置文件测试import功能
+ * @author liyinan
+ * @version 1.0
+ * @date 2015-03-26
+ */
+define(function (require) {
+    return {
+        importConfig: [
+            require('./config')
+        ]
+    };
+});
